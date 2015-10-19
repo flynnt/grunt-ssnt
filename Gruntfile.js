00563bd9553c075a78ce30f81f998f87428bff27
Ted Flynn <tflynn@nerdery.com>
2015-10-19 16:37:11 -0500

    Updates task registration in Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
     },
 
     // Configuration to be run (and then tested).
-    sst: {
+    ssnt: {
       default_options: {
         options: {
         },
@@ -65,7 +65,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'sst', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'ssnt', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint']);
